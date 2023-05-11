cb51c61146f4b084e2d6ece44f2668ab30bf4e06
Douglas Vieira <douglasdotv@hotmail.com>
2023-05-11 14:01:50 -0300

    Add toast with welcome message

--- app/src/main/java/br/com/dv/eastcity/MainActivity.kt
+++ app/src/main/java/br/com/dv/eastcity/MainActivity.kt
@@ -1,6 +1,14 @@
 package br.com.dv.eastcity
 
 import android.app.Activity
+import android.os.Bundle
+import android.widget.Toast
 
 class MainActivity : Activity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        Toast.makeText(this, "Welcome to East City!", Toast.LENGTH_SHORT).show()
+    }
+
 }
