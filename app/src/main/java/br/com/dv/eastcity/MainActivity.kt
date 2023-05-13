ea68c10103f00ccfb08a99e12e598da5fcc32c93
Douglas Vieira <douglasdotv@hotmail.com>
2023-05-13 15:14:16 -0300

    Use resource-based string and layout

--- app/src/main/java/br/com/dv/eastcity/MainActivity.kt
+++ app/src/main/java/br/com/dv/eastcity/MainActivity.kt
@@ -2,22 +2,14 @@ package br.com.dv.eastcity
 
 import android.app.Activity
 import android.os.Bundle
-import android.view.Gravity
-import android.widget.TextView
 import android.widget.Toast
 
 class MainActivity : Activity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
-        Toast.makeText(this, "Welcome to East City!", Toast.LENGTH_SHORT).show()
-
-        val view = TextView(this)
-        view.text = "Welcome to East City!"
-        view.gravity = Gravity.CENTER
-
-        setContentView(view)
+        Toast.makeText(this, R.string.welcome_message, Toast.LENGTH_SHORT).show()
+        setContentView(R.layout.activity_main)
     }
 
 }
