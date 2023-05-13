e70a2b6a8743794ece0a440807dcf89d86afcc0e
Douglas Vieira <douglasdotv@hotmail.com>
2023-05-13 15:04:48 -0300

    Add test view

--- app/src/main/java/br/com/dv/eastcity/MainActivity.kt
+++ app/src/main/java/br/com/dv/eastcity/MainActivity.kt
@@ -2,13 +2,22 @@ package br.com.dv.eastcity
 
 import android.app.Activity
 import android.os.Bundle
+import android.view.Gravity
+import android.widget.TextView
 import android.widget.Toast
 
 class MainActivity : Activity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         Toast.makeText(this, "Welcome to East City!", Toast.LENGTH_SHORT).show()
+
+        val view = TextView(this)
+        view.text = "Welcome to East City!"
+        view.gravity = Gravity.CENTER
+
+        setContentView(view)
     }
 
 }
