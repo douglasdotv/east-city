ec1fd6f78c49b912e95556ec4b20a93f1712643d
Douglas Vieira <douglasdotv@hotmail.com>
2023-05-16 03:58:21 -0300

    Change MainActivity to use RecyclerView

--- app/src/main/java/br/com/dv/eastcity/MainActivity.kt
+++ app/src/main/java/br/com/dv/eastcity/MainActivity.kt
@@ -3,6 +3,9 @@ package br.com.dv.eastcity
 import android.app.Activity
 import android.os.Bundle
 import android.widget.Toast
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import java.math.BigDecimal
 
 class MainActivity : Activity() {
 
@@ -13,13 +16,20 @@ class MainActivity : Activity() {
 
         setContentView(R.layout.activity_main)
 
-        /*
-        val name = findViewById<TextView>(R.id.name)
-        name.text = getString(R.string.item_name)
-
-        val description = findViewById<TextView>(R.id.description)
-        description.text = getString(R.string.tem_description)
-         */
+        val recyclerView = findViewById<RecyclerView>(R.id.recycler_view)
+        recyclerView.adapter = CarListAdapter(context = this, cars = listOf(
+            Car(
+                name = "Mazda RX-7",
+                description = "Mazda RX-7 Type RS-R",
+                price = BigDecimal("27737")
+            ),
+            Car(
+                name = "Fusca",
+                description = "Fusca 1972",
+                price = BigDecimal("10000")
+            ),
+        ))
+        recyclerView.layoutManager = LinearLayoutManager(this)
     }
 
 }
