a580c04f4d9d2dd2ea9b8a49158e6b067b3a3c79
Douglas Vieira <douglasdotv@hotmail.com>
2023-05-16 00:03:51 -0300

    Find view by id

--- app/src/main/java/br/com/dv/eastcity/MainActivity.kt
+++ app/src/main/java/br/com/dv/eastcity/MainActivity.kt
@@ -2,6 +2,7 @@ package br.com.dv.eastcity
 
 import android.app.Activity
 import android.os.Bundle
+import android.widget.TextView
 import android.widget.Toast
 
 class MainActivity : Activity() {
@@ -10,6 +11,10 @@ class MainActivity : Activity() {
         super.onCreate(savedInstanceState)
         Toast.makeText(this, R.string.welcome_message, Toast.LENGTH_SHORT).show()
         setContentView(R.layout.activity_main)
+        val name = findViewById<TextView>(R.id.name)
+        name.text = getString(R.string.first_item)
+        val description = findViewById<TextView>(R.id.description)
+        description.text = getString(R.string.description)
     }
 
 }
