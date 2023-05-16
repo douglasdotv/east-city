f7262dddd23cb957a7a472dc4d22937baa4bd80a
Douglas Vieira <douglasdotv@hotmail.com>
2023-05-16 01:14:11 -0300

    Begin RecyclerView impl, create Car model

---
+++ app/src/main/java/br/com/dv/eastcity/Car.kt
@@ -0,0 +1,9 @@
+package br.com.dv.eastcity
+
+import java.math.BigDecimal
+
+class Car(
+    val name: String,
+    val description: String,
+    val price: BigDecimal
+)
--- app/src/main/java/br/com/dv/eastcity/MainActivity.kt
+++ app/src/main/java/br/com/dv/eastcity/MainActivity.kt
@@ -2,19 +2,24 @@ package br.com.dv.eastcity
 
 import android.app.Activity
 import android.os.Bundle
-import android.widget.TextView
 import android.widget.Toast
 
 class MainActivity : Activity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         Toast.makeText(this, R.string.welcome_message, Toast.LENGTH_SHORT).show()
+
         setContentView(R.layout.activity_main)
+
+        /*
         val name = findViewById<TextView>(R.id.name)
-        name.text = getString(R.string.first_item)
+        name.text = getString(R.string.item_name)
+
         val description = findViewById<TextView>(R.id.description)
-        description.text = getString(R.string.description)
+        description.text = getString(R.string.tem_description)
+         */
     }
 
 }
