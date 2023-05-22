61c17f67efb37f2292cf11cc9f0375141921d928
Douglas Vieira <douglasdotv@hotmail.com>
2023-05-22 07:33:29 -0300

    Create CarFormActivity

---
+++ app/src/main/java/br/com/dv/eastcity/ui/activity/CarFormActivity.kt
@@ -0,0 +1,14 @@
+package br.com.dv.eastcity.ui.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import br.com.dv.eastcity.R
+
+class CarFormActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_car_form)
+    }
+
+}
